304da2e300ed1cfdc5c8b08dadfadbc22f7f2d65
Joshua <33937944+elementzprojects@users.noreply.github.com>
2020-02-15 14:18:04 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
 import pickle
-import keras
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout, Flatten
+from keras.layers import Dense, LSTM, Flatten
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 import re
@@ -26,12 +25,9 @@ except:
 
             processed.append(arr)
 
-    print(len(processed))
     processed = [sublist[0]
                  for sublist in itertools.groupby(sorted(processed))]
-    print(len(processed))
-    # print(processed)
-    # print(len(unique))
+
     unique_letters = []
     for uni in processed:
 
@@ -42,34 +38,19 @@ except:
 
     char2int = {c: i for i, c in enumerate(unique_letters)}
     int2char = {i: c for i, c in enumerate(unique_letters)}
-    # train_data = np.array([chartoint[char] for char in cities])
     training_data = []
     testing_data = []
-
-    def chartoint(words: str):
-        arr = []
-        for word in words:
-            for char in word:
-                arr.append(char2int[char])
-
-        return arr
-
     seq = 5
     xs = []
     ys = []
-    print(len(unique_letters))
-    # print(processed)
-
     for city in range(len(processed)):
 
         for i in range(0, len(processed[city]) - seq):
-            # print(processed[city][0:seq], processed[city][seq:])
             training_data.append(processed[city][i:i+seq])
             testing_data.append(processed[city][i+seq])
 
     for city in training_data:
         xs.append([char2int[l] / len(unique_letters) for l in city])
-        # print(xs)
 
     for city in testing_data:
         ys.append([char2int[l] for l in city])
